89a6f33fe5273c10840a85aaae6a381824eb1a81
Michal Migurski <mike@teczno.com>
2017-03-10 15:01:32 -0800

    Shrank the query substantially

--- code/import-collected-summaries.py
+++ code/import-collected-summaries.py
@@ -32,23 +32,16 @@ def summarize_country_coverage(db, iso_a2):
     '''
     print(iso_a2, '...')
     db.execute('''
-        WITH
+        SELECT
             --
-            -- .1x.1 box IDs of Natural Earth coverage, with GPWv4 population.
-            --
-            ne_boxes AS (
-            SELECT box.id, ne.name, gpw.population
-            FROM ne_50m_admin_0_countries as ne, gpwv4_2015 as gpw, boxes as box
-            WHERE ne.iso_a2 = %s
-              AND ne.iso_a2 = gpw.iso_a2
-              AND gpw.box_id = box.id
-              AND box.size = 0.1
-            ),
-            --
-            -- .1x.1 boxes of OpenAddresses counts with box IDs.
-            --
-            oa_boxes AS (
-            SELECT box.id, SUM(oa.count) AS count
+            min(count/population) AS min_cpp,
+            avg(count/population) AS avg_cpp,
+            max(count/population) AS max_cpp,
+            stddev_pop(count/population) AS std_cpp,
+            count(count/population) AS count_cpp
+
+        FROM (
+            SELECT box.id, SUM(oa.count) AS count, gpw.population
             FROM world_summaries as oa, gpwv4_2015 as gpw, boxes as box
             WHERE oa.iso_a2 = %s
               AND oa.iso_a2 = gpw.iso_a2
@@ -57,22 +50,10 @@ def summarize_country_coverage(db, iso_a2):
               AND box.lon = oa.lon
               AND box.size = oa.size
               AND box.size = 0.1
-            GROUP BY box.id
-            )
-        
-        SELECT
-            --
-            ne_boxes.name,
-            min(oa_boxes.count/ne_boxes.population) AS min_cpp,
-            avg(oa_boxes.count/ne_boxes.population) AS avg_cpp,
-            max(oa_boxes.count/ne_boxes.population) AS max_cpp,
-            stddev_pop(oa_boxes.count/ne_boxes.population) AS std_cpp,
-            count(oa_boxes.count/ne_boxes.population) AS count_cpp
-        FROM oa_boxes LEFT JOIN ne_boxes
-        ON ne_boxes.id = oa_boxes.id
-        GROUP BY ne_boxes.name
+            GROUP BY box.id, gpw.population
+            ) AS oa
         ''',
-        (iso_a2, iso_a2))
+        (iso_a2, ))
     
     print(db.fetchone())
     return
