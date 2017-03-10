ccddb9b372b0aeb7bd9884afb6f5dab3e276fe0f
Michal Migurski <mike@teczno.com>
2017-03-10 15:44:50 -0800

    Pulling from complete global collections now

--- code/import-collected-summaries.py
+++ code/import-collected-summaries.py
@@ -7,25 +7,27 @@ def stream_summary_files(start_url):
     '''
     '''
     index = requests.get(start_url).json()
-    collection_url = urljoin(start_url, index['collections']['global']['sa']['url'])
-    print('Downloading', collection_url, '...', file=sys.stderr)
-
-    collection = zipfile.ZipFile(remote.RemoteFileObject(collection_url))
     
-    for name in collection.namelist():
-        # CSV file paths look like "summary/{iso}/{etc}.csv"
-        iso_a2 = os.path.relpath(name, 'summary').split(os.path.sep)[0].upper()
-        _, ext = os.path.splitext(name)
+    for collection in index['collections']['global'].values():
+        collection_url = collection['url']
+        print('Downloading', collection_url, '...', file=sys.stderr)
+
+        collection = zipfile.ZipFile(remote.RemoteFileObject(collection_url, block_size=256*1024))
+    
+        for name in sorted(collection.namelist()):
+            # CSV file paths look like "summary/{iso}/{etc}.csv"
+            iso_a2 = os.path.relpath(name, 'summary').split(os.path.sep)[0].upper()
+            _, ext = os.path.splitext(name)
         
-        if (iso_a2 == '..') or (ext != '.csv'):
-            continue # Need a CSV file for the requested ISO code.
+            if (iso_a2 == '..') or (ext != '.csv'):
+                continue # Need a CSV file for the requested ISO code.
 
-        for row in csv.DictReader(collection.open(name)):
-            lon, lat = float(row['lon']), float(row['lat'])
-            count, geom_wkt = int(row['count']), row['area']
+            for row in csv.DictReader(collection.open(name)):
+                lon, lat = float(row['lon']), float(row['lat'])
+                count, geom_wkt = int(row['count']), row['area']
 
-            print(iso_a2, count, 'in', lon, lat)
-            yield (iso_a2, count, lon, lat, 0.1)
+                print(iso_a2, count, 'in', lon, lat)
+                yield (iso_a2, count, lon, lat, 0.1)
 
 start_url = 'https://results.openaddresses.io/index.json'
 summaries = collections.defaultdict(lambda: collections.defaultdict(int))
