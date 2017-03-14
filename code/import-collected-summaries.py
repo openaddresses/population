4a1d6a12e46210a639abd093020075e668cc6068
Michal Migurski <mike@teczno.com>
2017-03-14 11:42:21 -0700

    Started using area coverage intersections and attenuating outliers

--- code/import-collected-summaries.py
+++ code/import-collected-summaries.py
@@ -3,6 +3,8 @@ from __future__ import print_function, division
 import os, sys, remote, requests, zipfile, csv, psycopg2, collections, math
 from urlparse import urljoin
 
+exclude_usa = False
+
 def stream_summary_files(start_url):
     '''
     '''
@@ -23,6 +25,9 @@ def stream_summary_files(start_url):
             if (iso_a2 == '..') or (ext != '.csv'):
                 continue # Need a CSV file for the requested ISO code.
             
+            if iso_a2 == 'US' and exclude_usa:
+                continue
+
             namelist.append(name)
     
         for (index, name) in enumerate(sorted(namelist)):
@@ -49,10 +54,12 @@ with psycopg2.connect(os.environ['DATABASE_URL']) as conn:
             counts = list()
             
             db.execute('''SELECT boxes.lon, boxes.lat, boxes.size, gpwv4_2015.population
-                          FROM boxes, gpwv4_2015
+                          FROM boxes, gpwv4_2015, areas
                           WHERE gpwv4_2015.iso_a2 = %s
+                            AND areas.iso_a2 = gpwv4_2015.iso_a2
                             AND boxes.id = gpwv4_2015.box_id
-                            AND gpwv4_2015.population >= 1''',
+                            AND boxes.size = 0.1
+                            AND ST_Intersects(boxes.geom, areas.geom)''',
                        (iso_a2, ))
             
             for (lon, lat, size, population) in db.fetchall():
@@ -60,17 +67,41 @@ with psycopg2.connect(os.environ['DATABASE_URL']) as conn:
                     continue
                 
                 count = summaries[iso_a2][(lon, lat, size)]
-                counts.append(count/population)
+                if population:
+                    counts.append(count/population)
+                else:
+                    counts.append(0)
             
             if not counts:
                 continue
 
+            #
+            # Calculate real statistics
+            #
+            counts.sort()
             median = counts[len(counts) // 2]
             mean = sum([c/len(counts) for c in counts])
             deviations = [(c - mean) ** 2 for c in counts]
             variance = sum([d/len(deviations) for d in deviations])
             std_dev = math.sqrt(variance)
             
+            print(iso_a2, '- mean:', mean, 'median:', median, 'stddev:', std_dev, 'min:', min(counts), 'max:', max(counts))
+            
+            #
+            # Real statistics have some wacky outliers, so cut off the top and
+            # bottom 2% and try again. This attenuates the deviations a bit.
+            #
+            cutoff1 = int(len(counts) * 0.02)
+            cutoff2 = int(len(counts) * 0.98)
+            _counts = counts[cutoff1:cutoff2]
+            _median = _counts[len(_counts) // 2]
+            _mean = sum([c/len(_counts) for c in _counts])
+            _deviations = [(c - _mean) ** 2 for c in _counts]
+            _variance = sum([d/len(_deviations) for d in _deviations])
+            _std_dev = math.sqrt(_variance)
+            
+            print(iso_a2, '- _mean:', _mean, '_median:', _median, '_stddev:', _std_dev, '_min:', min(_counts), '_max:', max(_counts))
+            
             db.execute('''UPDATE areas SET cpp_min = %s, cpp_max = %s, cpp_avg = %s,
                           cpp_med = %s, cpp_stddev = %s WHERE iso_a2 = %s''',
-                       (min(counts), max(counts), mean, median, std_dev, iso_a2))
+                       (min(counts), max(counts), _mean, _median, _std_dev, iso_a2))
